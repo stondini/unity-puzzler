0466eb14c0ea73db85ad7ee74726a124c2d846af
Stephane Tondini <stondini@gmail.com>
2018-04-09 21:49:56 +0200

    UI

--- vrnd-puzzler/Assets/Scripts/GameLogic.cs
+++ vrnd-puzzler/Assets/Scripts/GameLogic.cs
@@ -7,6 +7,7 @@ public class GameLogic : MonoBehaviour
 
 	public void ToggleUI()
 	{
+        Debug.Log(startUI);
 		startUI.SetActive(!startUI.activeSelf);
 		restartUI.SetActive(!restartUI.activeSelf);
 	}
